6f3feef4681df1ee2af5ad0b2d0be24166da4b2b
mostafa <mtgwely0100@gmail.com>
2020-03-26 22:59:16 +0200

    adding ddl

---
+++ src/dropdown.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class DropDown extends Component {
+  state = {
+    ddlClass: 'share-dropdown ',
+    ddlModel: null
+  };
+  ddlToggle = () => {
+    const { ddlClass } = this.state
+    let enabled = ddlClass.includes('enabled') ? 1 : 0
+    this.setState({
+      ddlClass: enabled
+        ? ddlClass.replace('enabled', '')
+        : ddlClass + 'enabled'
+    })
+  };
+  itemSelected= (item) => {
+    this.setState({ddlModel: item})
+    this.ddlToggle()
+    this.props.onItemChange(item);
+  }
+
+  render() {
+    const { items, textValue} = this.props
+    return (
+      <React.Fragment>
+        <div className={this.state.ddlClass}>
+          <button
+            className='share-button share-rounded share-dropdown-toggle share-primary-bg'
+            onClick={this.ddlToggle}
+            data-toggle='dropdown'>{textValue}</button>
+          <div className='share-dropdown-menu'>
+            {items.map((item) => (<a onClick={() => this.itemSelected(item)}
+              className='share-dropdown-menu-item'
+              key={item}>{item}</a>))}
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default DropDown
