44b46dc1d453c063ffdf45d309b4e4a58ffa0806
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2020-03-26 19:02:35 +0200

    finial touch

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'share'
+import Loading from "share";
+import "./css/share.min.css";
 
 export default class App extends Component {
-  render () {
+  state = {
+    loading: false,
+    snackbar: false
+  };
+
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        {/* Loading Example
+        <button
+          className="share-button share-accent-bg"
+          onClick={() => this.setState({ loading: true })}
+        >
+          Show Loading
+        </button>
+        <Loading present={this.state.loading} /> */}
+
+        <button
+          className="share-button share-accent-bg"
+          onClick={() => this.setState({ snackbar: !this.state.snackbar })}
+        >
+          Show snackbar
+        </button>
+        <Loading
+          message="hello"
+          duration="3000"
+          present={this.state.snackbar}
+          onDurationEnd={() => this.setState({ snackbar: !this.state.snackbar })}
+
+        />
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,111 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
+const Loading = props => {
+  if (props.present) {
+    return (
+      <div id="share-loading" className="share-loading enabled">
+        <div className="share-loading-spinner"></div>
+      </div>
+    );
+  } else {
+    return (
+      <div id="share-loading" className="share-loading disabled">
+        <div className="share-loading-spinner"></div>
+      </div>
+    );
+  }
+};
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+class Snackbar extends Component {
+  state = {
+    show: true,
+    classes: "share-snackbar enabled"
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: false });
+      this.props.onDurationEnd;
+    }, this.props.duration);
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    let button = null ;
+    const isLoggedIn = this.state.show;
+    if (isLoggedIn) {
+      button = (
+        <div id="share-snackbar" className={this.state.classes}>
+          <span className="share-snackbar-message">{this.props.message}</span>
+        </div>
+      );
+    } else {
+      button = <div></div>;
+    }
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return button;
   }
 }
+
+// const Snackbar = props => {
+//   let classes = "share-snackbar ";
+//   let enabled = props.present;
+//   if (enabled === true) {
+//     classes += "enabled";
+//   } else {
+//     classes = "share-snackbar";
+//   }
+
+//   setTimeout(() => (enabled = false), props.duration);
+//   return (
+
+//   );
+// };
+
+// function() {
+// 	var element = null;
+
+// 	var init = function() {
+// 		document.addEventListener('DOMContentLoaded', function() {
+// 			// Need to add snackbar html to the DOM
+// 			var el = document.createElement("div");
+// 			el.id = 'share-loading';
+// 			el.classList.add('share-loading','disabled');
+
+// 			// Create the message node
+// 			var spinnerNode = document.createElement("div");
+// 			spinnerNode.classList.add('share-loading-spinner');
+// 			el.appendChild(spinnerNode);
+
+// 			document.body.appendChild(el);
+// 		});
+// 	}();
+
+// 	function present() {
+// 		// Show the loading overlay
+// 		if (!element) {
+// 			element = document.getElementById('share-loading');
+// 		}
+
+// 		// Add the msg
+// 		element.classList.remove('disabled');
+// 		element.classList.add('enabled');
+// 	}
+
+// 	function dismiss() {
+// 		element.classList.remove('enabled');
+// 		// Listen for transition end event
+// 		element.addEventListener( 'transitionend', onExitComplete );
+// 	}
+
+// 	function onExitComplete(e) {
+// 		element.removeEventListener( 'transitionend', onExitComplete );
+// 		element.classList.add('disabled');
+// 	}
+
+// 	return {
+// 		present: present,
+// 		dismiss: dismiss
+// 	}
+// }();
+
+export default Snackbar;
