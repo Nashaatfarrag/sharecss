db8749239c4b85413753f2b6b753fd8f3a142b00
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2020-03-27 01:50:23 +0200

    update readme

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # share
 
-> 
+>
 
 [![NPM](https://img.shields.io/npm/v/share.svg)](https://www.npmjs.com/package/share) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+share is a micro React JS framework with multiple reusable components ... attached with code a react project at example folder to illustrate the use of this frame work to run it simply go to
+
+```bash
+> cd /example
+> npm start
+```
+
+![example](./src/lSIxf5tRdk.gif)
+
+to import and use this frame work at your project run
 
 ## Install
 
@@ -13,19 +24,14 @@ npm install --save share
 ## Usage
 
 ```jsx
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import MyComponent from 'share'
+import share from "share";
 
 class Example extends Component {
-  render () {
-    return (
-      <MyComponent />
-    )
+  render() {
+    return <share.MyComponent />;
   }
 }
 ```
 
-## License
-
-MIT © [Nashaatfarrag](https://github.com/Nashaatfarrag)
--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import share from 'share'
-import './css/share.min.css'
+import share from "share";
+import "./css/share.min.css";
 
 export default class App extends Component {
   state = {
@@ -12,71 +12,71 @@ export default class App extends Component {
     to: null
   };
 
-  handleItemChange= (item) => {
-    this.setState({to: Number(item) === NaN ? '3' : String(item)})
-  }
+  handleItemChange = item => {
+    this.setState({ to: Number(item) === null ? "3" : String(item) });
+  };
   render() {
     return (
       <div>
         {/* Loading Example */}
         <button
-          className='share-button share-accent-bg'
+          className="share-button share-accent-bg"
           onClick={() => this.setState({ loading: true })}
         >
           Show Loading
         </button>
         <share.Loading present={this.state.loading} />
         <button
-          className='share-button share-accent-bg'
+          className="share-button share-accent-bg"
           onClick={() => this.setState({ snackbar: !this.state.snackbar })}
         >
           Show snackbar
         </button>
         {this.state.snackbar && (
-          <share.Snackbar fadeIn='20' message='hello' duration='3000' />
+          <share.Snackbar fadeIn="20" message="hello" duration="3000" />
         )}
         <button
-          className='share-button share-accent-bg'
+          className="share-button share-accent-bg"
           onClick={() => this.setState({ sidenav: !this.state.sidenav })}
         >
           Show Navbar
         </button>
         <share.Sidenav
-          buttonText='close'
+          buttonText="close"
           present={this.state.sidenav}
           onDismissClicked={() =>
             this.setState({ sidenav: !this.state.sidenav })
           }
-          right='true'
-          insideText='Sed vel urna ac purus luctus convallis venenatis eget magna. Proin
+          right="true"
+          insideText="Sed vel urna ac purus luctus convallis venenatis eget magna. Proin
             vestibulum feugiat dictum. Duis vehicula, arcu vitae dapibus
             tincidunt, dui leo pulvinar nibh, a suscipit ex purus in sem. Fusce
             auctor tincidunt arcu. Integer placerat sem vel ipsum vehicula, vel
             luctus erat pretium. In egestas erat eros, in pellentesque orci
-            imperdiet ut. Suspendisse aliquam hendrerit risus a vestibulum.'
+            imperdiet ut. Suspendisse aliquam hendrerit risus a vestibulum."
         />
         <button
-          className='share-button share-accent-bg'
+          className="share-button share-accent-bg"
           onClick={() => this.setState({ modal: !this.state.modal })}
         >
           Show modal
         </button>
         <share.Modal
           content={
-            <div className='share-grid share-responsive'>
-              <div className='share-gu-1-5 share-p-1'>Hi</div>
-              <div className='share-gu-1-5 share-p-1'>There</div>
-              <div className='share-gu-1-5 share-p-1'>You</div>
-              <div className='share-gu-1-5 share-p-1'>Big</div>
-              <div className='share-gu-1-5 share-p-1'>Fella</div>
+            <div className="share-grid share-responsive">
+              <div className="share-gu-1-5 share-p-1">Hi</div>
+              <div className="share-gu-1-5 share-p-1">There</div>
+              <div className="share-gu-1-5 share-p-1">You</div>
+              <div className="share-gu-1-5 share-p-1">Big</div>
+              <div className="share-gu-1-5 share-p-1">Fella</div>
             </div>
           }
-          dismissButtonText='dismiss'
+          dismissButtonText="dismiss"
           present={this.state.modal}
           onDismissClicked={() => this.setState({ modal: !this.state.modal })}
         />
         <share.Slide
-          heading='click me'
+          heading="click me"
           content={
             <div>
               <p>Hi</p>
@@ -88,9 +88,9 @@ export default class App extends Component {
         />
 
         <share.DropDown
-          items={['one', 'two', 'three', 'ffff']}
-          textValue='menu'
-          onItemChange={(item) => this.handleItemChange(item)}
+          items={["one", "two", "three", "ffff"]}
+          textValue="menu"
+          onItemChange={item => this.handleItemChange(item)}
           returnIndex={true}
         />
 
@@ -102,6 +102,6 @@ export default class App extends Component {
           to={this.state.to}
         />
       </div>
-    )
+    );
   }
 }
