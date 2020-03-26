523b39cc785e660645075d5fb43c83c8aaf6b634
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2020-03-26 19:47:06 +0200

    upate sidenav

---
+++ src/sidenav.jsx
@@ -0,0 +1,14 @@
+
+const Sidenav = props => {
+
+    return (
+<div className="share-sidenav">
+    <div className="share-sidenav-menu">
+        <div className="share-p-2">
+            <button className="share-button share-rounded">Close</button>
+        </div>
+    </div>
+</div>
+      );
+
+}
